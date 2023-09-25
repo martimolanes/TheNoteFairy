423528284bd00532077bb91abdd4f1eaf81c28f5
martinho <martinho.n03@gmail.com>
2023-09-25 17:49:11 +0300

    feat(tui): add possibility to logout and call a method in the bottom subwindow

--- simple_note_app/utils/tui.py
+++ simple_note_app/utils/tui.py
@@ -4,11 +4,29 @@ display menu and get user input
 import curses
 
 ENTER_KEY = 10
+FINNISH_OPTION = 2
 
 def menu(stdscr: curses.window, username: str):
     curses.curs_set(0)
     # Clear screen
     stdscr.clear()
+    
+    # make subwindow that occups all of the bottom middle of the screen
+    subwin_height = curses.LINES * 3 // 4
+    subwin_width = curses.COLS
+    subwin_y = curses.LINES - subwin_height
+    subwin_x = curses.COLS // 2 - subwin_width // 2
+
+    # Create a subwindow
+    subwin: curses.window = stdscr.subwin(subwin_height, subwin_width, subwin_y, subwin_x)
+    subwin.border()
+
+    # Add content to the subwindow
+    subwin.addstr(1, 1, "This is a subwindow")
+    subwin.addstr(3, 1, "Press any key to exit")
+
+    # Refresh the subwindow to show its content
+    subwin.refresh()
 
     # Set colors
     curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
@@ -43,12 +61,21 @@ def menu(stdscr: curses.window, username: str):
         elif key == curses.KEY_DOWN:
             current_option = (current_option + 1) % len(options)
         elif key == ENTER_KEY:
-            break
+            if current_option == 2:
+                break
+            action(subwin, current_option)
 
 
     # Clear screen and exit
     stdscr.clear()
     curses.endwin()
+
+def action(subwin: curses.window, option: int):
+    if option == 0:
+        subwin.addstr(3, 3, "Create a note")
+    elif option == 1:
+        subwin.addstr(3, 3, "Retrieve a note")
+    subwin.refresh()
 
 # Run main function
 def run(username: str):
