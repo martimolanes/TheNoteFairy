efb6e9119cc8b59a50528ec472345ab83270797e
martinho <martinho.n03@gmail.com>
2023-09-27 17:33:51 +0300

    fix(tui): interesting bug when trying to display in the right-bottom corner a char

--- simple_note_app/tui/utils.py
+++ simple_note_app/tui/utils.py
@@ -1,7 +1,8 @@
 import curses
+import _curses
 from tui.constants import TOP_LEFT_CHAR, TOP_RIGHT_CHAR, BOTTOM_LEFT_CHAR, BOTTOM_RIGHT_CHAR
 
-def refresh_subwindow_border(subwin):
+def refresh_subwindow_border(subwin: curses.window):
     subwin_height = curses.LINES * 3 // 4
     subwin_width = curses.COLS
     subwin.border()
@@ -9,8 +10,12 @@ def refresh_subwindow_border(subwin):
     subwin.addch(0, 0, TOP_LEFT_CHAR)
     subwin.addch(0, subwin_width - 1, TOP_RIGHT_CHAR)
     subwin.addch(subwin_height - 1, 0, BOTTOM_LEFT_CHAR)
-    #FIXME: only bottom right corner not WORKING
-    #subwin.addch(subwin_height-1, subwin_width-1, BOTTOM_RIGHT_CHAR)
+    # This is a cotrolled crash
+    # https://stackoverflow.com/questions/36387625/curses-fails-when-calling-addch-on-the-bottom-right-corner
+    try:
+        subwin.addch(subwin_height-1, subwin_width-1, BOTTOM_RIGHT_CHAR)
+    except _curses.error:
+        pass
 
     subwin.addstr(0, curses.COLS // 2 - 6, " TheNoteFairy ")
     # Refresh the subwindow to show its content
